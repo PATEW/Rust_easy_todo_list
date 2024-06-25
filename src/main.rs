ca953742f96403c5b379c40842ae038bd6a269ea
PATEW <patew@my.erau.edu>
2024-06-25 15:54:37 -0500

    working: save generated json to file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod assignments;
-use std::vec;
+use std::{fs::File, io::{BufWriter, Write}, vec};
 use serde_derive::Serialize;
 
 use assignments::Assignment;
@@ -40,34 +40,7 @@ fn main() {
 
     // if calendar exists, use it
     // else create new one:
-    let assignment1: Assignment = Assignment::new(String::from("test one"), String::from("basics of biology"), String::from("01/01/24 10:00:00"));
-    let assignment2: Assignment = Assignment::new(String::from("my meeting"), String::from("daily stand up"), String::from("01/01/24 08:00:00"));
-    let assignment3: Assignment = Assignment::new(String::from("homework 3"), String::from("working on code"), String::from("01/02/24 11:00:00"));
-    let assignment4: Assignment = Assignment::new(String::from("my meeting"), String::from("daily stand up"), String::from("01/02/24 14:00:00"));
-    let assignment5: Assignment = Assignment::new(String::from("my feb meeting"), String::from("blah blah"), String::from("02/01/24 09:00:00"));
-
-    let day_one: CalDay = CalDay {date: "01/01/24".to_string(), assignments: vec![assignment1, assignment2]};
-    let day_two: CalDay = CalDay {date: "01/02/24".to_string(), assignments: vec![assignment3, assignment4]};
-    let day_three: CalDay = CalDay {date: "02/01/24".to_string(), assignments: vec![assignment5]};
- 
-    let month_one: CalMonth = CalMonth {name: "January".to_string(), days: vec![day_one, day_two]};
-    let month_two: CalMonth = CalMonth {name: "February".to_string(), days: vec![day_three]};
-
-    let calendar: Calendar = Calendar {
-        year: "2024".to_string(),
-        months: vec![month_one, month_two],
-    };
-
-    let json_str = match serde_json::to_string(&calendar) {
-        Ok(v) => v,
-        Err(_) => {
-            eprintln!("Unable to load data");
-            panic!("JSON brokey or something");
-        }
-    };
-
-    println!("{}", json_str);
-
+    create_default_calendar();
 
 
     current_state = AppState::MainMenu;
@@ -104,7 +77,41 @@ pub fn create_item() {
 }
 
 pub fn create_default_calendar() {
+    // this is just gross looking for testing, will be much smaller after other features work
+    let assignment1: Assignment = Assignment::new(String::from("test one"), String::from("basics of biology"), String::from("01/01/24 10:00:00"));
+    let assignment2: Assignment = Assignment::new(String::from("my meeting"), String::from("daily stand up"), String::from("01/01/24 08:00:00"));
+    let assignment3: Assignment = Assignment::new(String::from("homework 3"), String::from("working on code"), String::from("01/02/24 11:00:00"));
+    let assignment4: Assignment = Assignment::new(String::from("my meeting"), String::from("daily stand up"), String::from("01/02/24 14:00:00"));
+    let assignment5: Assignment = Assignment::new(String::from("my feb meeting"), String::from("blah blah"), String::from("02/01/24 09:00:00"));
+    let day_one: CalDay = CalDay {date: "01/01/24".to_string(), assignments: vec![assignment1, assignment2]};
+    let day_two: CalDay = CalDay {date: "01/02/24".to_string(), assignments: vec![assignment3, assignment4]};
+    let day_three: CalDay = CalDay {date: "02/01/24".to_string(), assignments: vec![assignment5]};
+    let month_one: CalMonth = CalMonth {name: "January".to_string(), days: vec![day_one, day_two]};
+    let month_two: CalMonth = CalMonth {name: "February".to_string(), days: vec![day_three]};
 
+    let calendar: Calendar = Calendar {
+        year: "2024".to_string(),
+        months: vec![month_one, month_two],
+    };
+
+    let json_str = match serde_json::to_string(&calendar) {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("Unable to load data");
+            panic!("JSON brokey or something");
+        }
+    };
+
+    println!("loaded calendar: {}", json_str);
+    let print_result: std::io::Result<()> = write_to_json(json_str);
+}
+
+fn write_to_json(json_str: String) -> std::io::Result<()> {
+    let file = File::create("data.json")?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &json_str)?;
+    writer.flush()?;
+    Ok(())
 }
 
 fn read_int() -> i32 {
